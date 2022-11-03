8423c5b7d7197c9822533532e5d1cc2c8ee26571
MuchamadTaufik <muhamadtaufikm10@gmail.com>
2022-11-04 00:51:11 +0700

    View Login & Registration

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Models\Post;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
 
 use App\Models\Category;
-use App\Models\User;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -44,3 +44,6 @@ Route::get('/categories', function(){
         'categories' => Category::all()
     ]);
 });
+
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index']);
